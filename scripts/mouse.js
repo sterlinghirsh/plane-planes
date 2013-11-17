1eafa6f82793b1b368d329bc11aa8a06cd93b696
tawnos <tawnos@gmail.com>
2013-11-17 15:51:46 -0800

    add a mouse object, not yet tested

---
+++ scripts/mouse.js
@@ -0,0 +1,32 @@
+﻿document.addEventListener('mousemove', Mouse.onMouseMove, false);
+document.addEventListener('mousedown', Mouse.onMouseDown, false);
+document.addEventListener('mouseup', Mouse.onMouseUp, false);
+
+var Mouse = {
+    _x: 0,
+    _y: 0,
+    _pressed: {},
+
+    LEFT_MOUSE: 1,
+    RIGHT_MOUSE: 2,
+
+    onMouseMove: function (event) {
+        _x = WIDTH_HALF / 2 * ((event.clientX / SCREEN_WIDTH) * 2 - 1);
+        _y = HEIGHT_HALF / 2 * (-(event.clientY / SCREEN_HEIGHT) * 2 + 1);
+    },
+
+    onMouseDown: function(event) {
+        event.preventDefault();
+        event.stopPropogation();
+        this._pressed[event.which] = true;
+    },
+
+    onMouseUp: function (event) {
+        event.preventDefault();
+        event.stopPropogation();
+        this._pressed[event.which] = false;
+    },
+
+    getX: function() { return _x; },
+    getY: function() { return _y; }
+};
